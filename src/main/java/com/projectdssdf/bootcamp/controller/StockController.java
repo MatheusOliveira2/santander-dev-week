03651a5da458c9cb11c30c5b0bd91710dbf430b4
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-26 22:01:02 -0300

    sotocks controller

--- src/main/java/com/projectdssdf/bootcamp/controller/StockController.java
+++ src/main/java/com/projectdssdf/bootcamp/controller/StockController.java
@@ -1,2 +1,50 @@
-package com.projectdssdf.bootcamp.controller;public class StockController {
+package com.projectdssdf.bootcamp.controller;
+
+import com.projectdssdf.bootcamp.model.dto.StockDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/stock")
+public class StockController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> save(@RequestBody StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> update(@RequestBody StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findAll(){
+        List<StockDTO> list = new ArrayList<>();
+        StockDTO dto = new StockDTO();
+        dto.setId(1L);
+        dto.setName("Oi");
+        dto.setPrice(1.8);
+        dto.setVariation(10D);
+        dto.setDate(LocalDate.now());
+        list.add(dto);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> findById(@PathVariable Long id){
+        StockDTO dto = new StockDTO();
+        dto.setId(id);
+        dto.setName("Oi");
+        dto.setPrice(1.8);
+        dto.setVariation(10D);
+        dto.setDate(LocalDate.now());
+        return ResponseEntity.ok(dto);
+    }
+
 }
