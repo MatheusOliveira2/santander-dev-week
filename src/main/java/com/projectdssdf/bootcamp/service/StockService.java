df673cd89ecf580f15a953fe72bbdda3028ee69f
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-27 22:06:38 -0300

    Exception Handler

--- src/main/java/com/projectdssdf/bootcamp/service/StockService.java
+++ src/main/java/com/projectdssdf/bootcamp/service/StockService.java
@@ -1,13 +1,17 @@
 package com.projectdssdf.bootcamp.service;
 
 
+import com.projectdssdf.bootcamp.exception.BusinessException;
 import com.projectdssdf.bootcamp.mapper.StockMapper;
 import com.projectdssdf.bootcamp.model.Stock;
 import com.projectdssdf.bootcamp.model.dto.StockDTO;
 import com.projectdssdf.bootcamp.repository.StockRepository;
+import com.projectdssdf.bootcamp.util.MessageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 
 @Service
@@ -21,6 +25,11 @@ public class StockService {
 
     @Transactional
     public StockDTO save(StockDTO dto) {
+        Optional<Stock> optionalStock = repository.findByNameAndDate(dto.getName(),dto.getDate());
+        if(optionalStock.isPresent()){
+            throw new BusinessException(MessageUtils.STOCK_ALREADY_EXISTS);
+        }
+
         Stock stock = mapper.toEntity(dto);
         repository.save(stock);
         return mapper.toDto(stock);
