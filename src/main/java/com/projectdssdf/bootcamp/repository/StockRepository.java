aff092f96a65c997393453f6f288bf6ba7dd8237
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-27 21:14:11 -0300

    Sotck Repository

--- src/main/java/com/projectdssdf/bootcamp/repository/StockRepository.java
+++ src/main/java/com/projectdssdf/bootcamp/repository/StockRepository.java
@@ -1,2 +1,10 @@
-package com.projectdssdf.bootcamp.repository;public interface StockRepository {
+package com.projectdssdf.bootcamp.repository;
+
+import com.projectdssdf.bootcamp.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Long> {
 }
